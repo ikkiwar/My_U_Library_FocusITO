248467c67dd43124f135a330336da2a0b5da9194
ikkiwar <ikkiofbodom@gmail.com>
2022-04-09 02:23:26 -0600

    user and bokks endpoints created

--- my_u_library_backend/index.js
+++ my_u_library_backend/index.js
@@ -10,6 +10,14 @@ var CONNECTION_STRING = "mongodb://3.142.191.243:27017/?readPreference=primary&a
 var DATABASE_NAME = "MyULibrary";
 var database;
 
+var fileUpload = require('express-fileupload');
+var fs = require('fs');
+app.use(fileUpload());
+app.use('/Photos', Express.static(__dirname + '/Photos'));
+
+var cors = require('cors');
+app.use(cors());
+
 
 
 app.listen(49146, () => {
@@ -30,7 +38,152 @@ app.get('/api/role', (req, res) => {
             console.log(err);
             res.sendStatus(500);
         } else {
-            res.send(docs);
+            res.json(docs);
         }
     });
 });
+
+// Books
+
+app.get('/api/books', (req, res) => {
+    database.collection('books').find({}).toArray(function (err, docs) {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+        } else {
+            res.json(docs);
+        }
+    });
+});
+
+app.post('/api/books', (req, res) => {
+   database.collection("books").count({}, function (err, count) {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+        } else {
+            var book = req.body;
+            book.idBook = count + 1;
+
+            database.collection("books").insertOne(book, function (err, result) {
+                if (err) {
+                    console.log(err);
+                    res.sendStatus(500);
+                } else {
+                    res.json(book);
+                }
+            });
+        }
+    });
+});
+
+app.put('/api/books/:id', (req, res) => {
+
+            database.collection("books").updateOne(
+                {
+                    "idBook": parseInt(req.params.id)
+                },
+                {
+                    $set: {
+                        "title": req.body.title,
+                        "author": req.body.author,
+                        "published_year": req.body["published_year"],
+                        "genre": req.body.genre,
+                        "image": req.body.image,
+                        "copies": req.body.copies
+                    }
+                }
+
+            );
+            res.json("Book updated");
+
+});
+
+app.delete('/api/books/:id', (req, res) => {
+    database.collection("books").deleteOne(
+        {
+            "idBook": parseInt(req.params.id)
+        }
+    );
+    res.json("Book deleted");
+});
+
+app.post('/api/books/savefile', (req,res) => {
+    fs.writeFile("./Photos/"+req.files.file.name,
+        req.files.file.data,
+        function (err) {
+            if (err) {
+                console.log(err);
+                res.sendStatus(500);
+            } else {
+                res.json("File uploaded");
+            }
+        }
+        )
+})
+
+// Users
+
+app.get('/api/user', (req, res) => {
+    database.collection('user').find({}).toArray(function (err, docs) {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+        } else {
+            res.json(docs);
+        }
+    });
+});
+
+app.post('/api/user', (req, res) => {
+    database.collection("user").count({}, function (err, count) {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+        } else {
+            var user = req.body;
+            user.idUser = count + 1;
+
+            database.collection("user").insertOne(user, function (err, result) {
+                if (err) {
+                    console.log(err);
+                    res.sendStatus(500);
+                } else {
+                    res.json(user);
+                }
+            });
+        }
+    });
+});
+
+app.put('/api/user/:id', (req, res) => {
+
+    database.collection("user").updateOne(
+        {
+            "idUser": parseInt(req.params.id)
+        },
+        {
+            $set: {
+                "name": req.body.name,
+                "last_name": req.body["last_name"],
+                "email": req.body.email,
+                "password": req.body.password,
+                "idRole": req.body.idRole
+            }
+        }
+
+    );
+    res.json("User updated");
+
+});
+
+app.delete('/api/user/:id', (req, res) => {
+    database.collection("user").deleteOne(
+        {
+            "idUser": parseInt(req.params.id)
+        }
+    );
+    res.json("User deleted");
+});
+
+
