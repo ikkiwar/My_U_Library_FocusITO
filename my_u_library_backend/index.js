116658611b2bcca6b24ae600e81012494a99080a
ikkiwar <ikkiofbodom@gmail.com>
2022-04-09 21:46:38 -0600

    endpoints methods finish

--- my_u_library_backend/index.js
+++ my_u_library_backend/index.js
@@ -1,12 +1,13 @@
 var Express = require('express');
 var BodyParser = require('body-parser');
+require('dotenv').config();
 
 var app = Express();
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
 
 var MongoClient = require('mongodb').MongoClient;
-var CONNECTION_STRING = "mongodb://3.142.191.243:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+var CONNECTION_STRING = process.env.BASE_CONNECTION_CHAIN;
 var DATABASE_NAME = "MyULibrary";
 var database;
 
@@ -46,7 +47,8 @@ app.get('/api/role', (req, res) => {
 // Books
 
 app.get('/api/books', (req, res) => {
-    database.collection('books').find({}).toArray(function (err, docs) {
+
+    database.collection('books').find({ ...req.query}).toArray(function (err, docs) {
         if (err) {
             console.log(err);
             res.sendStatus(500);
@@ -125,7 +127,9 @@ app.post('/api/books/savefile', (req,res) => {
 // Users
 
 app.get('/api/user', (req, res) => {
-    database.collection('user').find({}).toArray(function (err, docs) {
+    var query = {};
+    req.query.email ? query.email = req.query.email : null;
+    database.collection('user').find({...query}).toArray(function (err, docs) {
         if (err) {
             console.log(err);
             res.sendStatus(500);
@@ -186,4 +190,85 @@ app.delete('/api/user/:id', (req, res) => {
     res.json("User deleted");
 });
 
+// borrowed books
 
+app.get('/api/borrowedbooks', (req, res) => {
+    var queryBody = {};
+    req.query.idUser ? queryBody.idUser = parseInt(req.query.idUser) : null;
+    req.query.idBook ? queryBody.idBook = parseInt(req.query.idBook) : null;
+    req.query.idBookStatus ? queryBody.idBookStatus = parseInt( req.query.idBookStatus): null;
+
+    database.collection('borrowedbooks').aggregate([
+
+        {$match: queryBody},
+        {
+        $lookup:{
+            from: 'user',
+            localField: 'idUser',
+            foreignField: 'idUser',
+            as: 'user'
+        }
+    },{
+        $lookup:{
+            from: 'books',
+            localField: 'idBook',
+            foreignField: 'idBook',
+            as: 'book'
+        }
+    },{
+        $lookup:{
+            from: 'bookstatus',
+            localField: 'idBookStatus',
+            foreignField: 'idBookStatus',
+            as: 'status'
+        }
+    },  {$unset: ["user.password"]},]).toArray(function (err, docs) {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+        } else {
+
+            res.json(docs);
+        }
+    });
+});
+
+app.post('/api/borrowedbooks', (req, res) => {
+    database.collection("borrowedbooks").count({}, function (err, count) {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+        } else {
+            var borrowed = req.body;
+            borrowed.idBorrowed = count + 1;
+
+            database.collection("borrowedbooks").insertOne(borrowed, function (err, result) {
+                if (err) {
+                    console.log(err);
+                    res.sendStatus(500);
+                } else {
+                    res.json(borrowed);
+                }
+            });
+        }
+    });
+});
+
+app.put('/api/borrowedbooks/:id', (req, res) => {
+
+    database.collection("borrowedbooks").updateOne(
+        {
+            "idBorrowed": parseInt(req.params.id)
+        },
+        {
+            $set: {
+                "idBook": req.body.idBook,
+                "idUser": req.body.idUser,
+                "idBookStatus": req.body.idBookStatus
+            }
+        }
+
+    );
+    res.json("Book updated");
+
+});
